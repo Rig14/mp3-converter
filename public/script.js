43a0933673a4525879a645b7ccf4bec7b0a9425d
ririvi <ririvi@taltech.ee>
2023-11-02 19:31:23 +0000

    foundations for signup page

--- public/script.js
+++ public/script.js
@@ -1,1 +1,17 @@
-console.log('Hello from script.js');
+const showPassword = (fieldID) => {
+    // shows the password in plain text instead on dots
+    const field = document.getElementById(fieldID);
+    field.type = 'text';
+};
+const hidePassword = (fieldID) => {
+    // hides the password and shows dots
+    const field = document.getElementById(fieldID);
+    field.type = 'password';
+};
+
+const processFormData = () => {
+    // get form data from the fields in the form
+    const form = document.getElementById('form');
+    const formData = new FormData(form);
+    console.log(formData);
+};
