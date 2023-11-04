835a11639005d10502033e8b2c6c33e20cec08d7
ririvi <ririvi@taltech.ee>
2023-11-04 10:42:14 +0000

    changed function definition

--- public/script.js
+++ public/script.js
@@ -1,17 +1,17 @@
-const showPassword = (fieldID) => {
+function showPassword(fieldID) {
     // shows the password in plain text instead on dots
     const field = document.getElementById(fieldID);
     field.type = 'text';
-};
-const hidePassword = (fieldID) => {
+}
+function hidePassword(fieldID) {
     // hides the password and shows dots
     const field = document.getElementById(fieldID);
     field.type = 'password';
-};
+}
 
-const processFormData = () => {
+function processFormData() {
     // get form data from the fields in the form
     const form = document.getElementById('form');
     const formData = new FormData(form);
     console.log(formData);
-};
+}
