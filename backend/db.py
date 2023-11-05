11a39d5555b9a6eddad301e7b4840d9e2ce2d26a
ririvi <ririvi@taltech.ee>
2023-11-05 16:34:12 +0000

    remove error when reinit new db

--- backend/db.py
+++ backend/db.py
@@ -6,6 +6,10 @@ def init_empty_db():
     """
     Initialize empty database
     """
+    # if database already exsists - delete it
+    if os.path.exists(os.path.join(os.path.dirname(__file__), "database.db")):
+        os.remove(os.path.join(os.path.dirname(__file__), "database.db"))
+
     os.chdir(os.path.dirname(__file__))
     con = sqlite3.connect(os.path.join(os.path.dirname(__file__), "database.db"))
     cur = con.cursor()
