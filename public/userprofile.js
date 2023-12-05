2d2ed0d6d4c3e6bc474f0edfe0f8fe33758d7680
ririvi <ririvi@taltech.ee>
2023-12-05 16:35:37 +0000

    user data changing functionality in the backend

--- public/userprofile.js
+++ public/userprofile.js
@@ -11,3 +11,45 @@ function load_user_profile() {
     user_profile_email.value = user_data.email;
 }
 load_user_profile();
+
+async function change_user_data() {
+    set_loading_animation();
+    const name = document.getElementById('user-profile-name').value;
+    const motd = document.getElementById('user-profile-message').value;
+    const email = document.getElementById('user-profile-email').value;
+    const password = document.getElementById('user-profile-password').value;
+    const token = localStorage.getItem('token');
+
+    const url = BACKEND_URL + '/api/change_user_data';
+
+    const data = {
+        name: name,
+        motd: motd,
+        email: email,
+        password: password,
+    };
+
+    const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: token,
+        },
+    });
+
+    if (response.status != 200) {
+        const data = await response.json();
+        set_error(data.error);
+        remove_loading_animation();
+    } else {
+        await locally_save_user_data();
+        remove_loading_animation();
+        window.location.reload();
+    }
+}
+
+function set_error(message) {
+    const error = document.getElementById('user-profile-error');
+    error.innerHTML = message;
+}
