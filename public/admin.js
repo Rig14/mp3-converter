4791103919b7aa45cf5e73882b8bb531cd14f7e3
ririvi <ririvi@taltech.ee>
2023-12-12 14:11:14 +0000

    blacklist now shows on the client side

--- public/admin.js
+++ public/admin.js
@@ -17,7 +17,17 @@ async function render_blacklist() {
         const blacklist_container = document.getElementById(
             'blacklist-container'
         );
-        blacklist_container.innerHTML = data;
+        data.items.forEach((item) => {
+            const blacklist_item = document.createElement('li');
+            blacklist_item.className = 'blacklist-item';
+            blacklist_item.innerHTML = `
+                <p class="blacklist-item-id">ID: ${item[0]}</p>
+                <p class="blacklist-item-content-url">Content URL: ${item[1]}</p>
+                <p class="blacklist-item-date">Date added: ${item[2]}</p>
+                <button class="blacklist-item-delete" onclick="delete_from_blacklist(${item.id})">Delete</button>
+            `;
+            blacklist_container.appendChild(blacklist_item);
+        });
     }
 }
 
